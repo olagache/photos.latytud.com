df7d01472426ca8d26a54f08afc882cd8e8afaf7
olagache <olagache@gmail.com>
2015-07-11 08:35:24 +0200

    Remove js/latytud.* from gitignore -> files are generated by gulp :(

---
+++ js/latytud.js
@@ -0,0 +1,29 @@
+angular.module('latytudPhotoApp', []);
+
+angular.module("latytudPhotoApp").controller("LeftMenuController", ["$scope", "latytudService", function($scope, latytudService) {
+  $scope.leftMenu;
+  $scope.activeMenu;
+
+  loadLeftMenu();
+
+  function loadLeftMenu() {
+    console.log("IN loadLeftMenu FROM LeftMenuController");
+    latytudService.fetchLeftMenu()
+      .success(function(data) {
+        $scope.leftMenu = data.leftMenu;
+        $scope.activeMenu = data.activeMenu;
+        console.log("SUCCESS IN loadLeftMenu " + this.leftMenu);
+      })
+      .error(function(data, status, headers, config) {
+        console.log("ERROR IN loadLeftMenu");
+    });
+  }
+}])
+
+angular.module("latytudPhotoApp").factory("latytudService", ["$http", function($http) {
+    return {
+      fetchLeftMenu : function() {
+        return $http.get('/json/leftMenu.json')
+      }
+    }
+}])
